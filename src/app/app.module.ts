12a58f81568536d2cc6588742cff4c2848f473e1
Madhukara.Hiriadka <Madhukara.Hiriadka@LTPMAN022149586>
2018-04-01 12:58:04 +0530

    application module added and refactoring other code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BannerComponent } from './components/home/banner.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/layout/header.component';
-import { FooterComponent } from './components/layout/footer.component';
-import { Section1Component } from './components/home/section1.component';
-import { Section2Component } from './components/home/section2.component';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BannerComponent } from './home/banner.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from './layout/footer.component';
+import { Section1Component } from './home/section1.component';
+import { Section2Component } from './home/section2.component';
+import { ApplicationComponent } from './application/application.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
     FooterComponent,
     Section1Component,
     Section2Component,
-    HomeComponent
+    HomeComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
