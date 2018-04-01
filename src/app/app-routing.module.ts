8e1b5f7aeb229cb85875a51416116656aa088ee0
Madhukara.Hiriadka <Madhukara.Hiriadka@LTPMAN022149586>
2018-04-01 13:14:40 +0530

    Minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ApplicationComponent } from './application/application.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'apply', component: ApplicationComponent },
 ];
 
 @NgModule({
